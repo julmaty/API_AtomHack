046702ed02f3a654723d487806d5cf695baafec0
Матюнина Юлия <yorkshirelove@yandex.ru>
2024-03-21 11:37:25 +0300

    newController

---
+++ API_AtomHack/Controllers/SystemController.cs
@@ -0,0 +1,26 @@
+﻿using API_AtomHack.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_AtomHack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public SystemController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Messages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Model.System>>> Get()
+        {
+            return await _context.Systems.ToListAsync();
+        }
+    }
+}
